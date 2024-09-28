c4bdf0dd3f53f956c53a50be045fe1e3d0309277
Pierre Erraud <pierre.erraud@manomano.com>
2024-09-28 14:54:08 +0200

    feat: add NewSecuredClientWithResponses()

--- finout.go
+++ finout.go
@@ -43,3 +43,36 @@ func NewSecuredClient(clientID, secretKey string) (*Client, error) {
 		// }),
 	)
 }
+
+func NewSecuredClientWithResponses(clientID, secretKey string) (*ClientWithResponses, error) {
+	headerClientID, err := securityprovider.NewSecurityProviderApiKey("header", "x-finout-client-id", clientID)
+	if err != nil {
+		return nil, err
+	}
+
+	headerSecretKey, err := securityprovider.NewSecurityProviderApiKey("header", "x-finout-secret-key", secretKey)
+	if err != nil {
+		return nil, err
+	}
+
+	return NewClientWithResponses(
+		DefaultBaseURL,
+		WithRequestEditorFn(func(ctx context.Context, req *http.Request) error {
+			req.Header.Set("Accept", "application/json")
+			return nil
+		}),
+		WithRequestEditorFn(headerClientID.Intercept),
+		WithRequestEditorFn(headerSecretKey.Intercept),
+		// WithRequestEditorFn(func(ctx context.Context, req *http.Request) error {
+		// 	dump, err := httputil.DumpRequestOut(req, true)
+
+		// 	if err != nil {
+		// 		return err
+		// 	}
+
+		// 	log.Printf("\n\n%q", dump)
+
+		// 	return nil
+		// }),
+	)
+}
